a001e6821122f758e2fee3a6a2007c6a4de52edd
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-14 13:37:51 -0300

    Add tailwindcss config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,29 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      medium: 18,
+      large: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+
+    colors: {
+      black: "#000000",
+      white: "#FFFFFF",
+
+      "gray-900": "#121214",
+      "gray-800": "#202024",
+      "gray-400": "#7C7C8A",
+      "gray-200": "#E1E1E6",
+      "gray-100": "#C4C4CC",
+
+      "cyan-500": "#81D8F7",
+      "cyan-300": "#9BE1FB",
+    },
+
     extend: {
       fontFamily: {
         sans: "Inter, sans-serif",
